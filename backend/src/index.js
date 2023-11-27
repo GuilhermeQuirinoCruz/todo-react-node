244ec070a3d03979c54bb73fc9f3fbfc76e73d7c
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-27 19:28:50 -0300

    cors adicionado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 // Allow cross-domain
 app.use((req, res, next) => {
